a9cac52ff4651ad3e9f4beda79d946a114644075
Ian Esling <ian.esling@gmail.com>
2020-05-13 14:05:19 +0100

    see what difference this Application class makes

---
+++ slack-reader/src/main/java/dev/number6/slackreader/Application.java
@@ -0,0 +1,9 @@
+package dev.number6.slackreader;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
